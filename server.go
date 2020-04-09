dd7ce255dabc47b78b99e305e10d9f1af814c3a5
bruteforce1414 <bruteforce1414@gmail.com>
2020-04-09 16:16:53 +0300

    test: add implementer for post with json

--- server.go
+++ server.go
@@ -13,7 +13,17 @@ func main() {
 	//http://localhost:1323/api/user?name=name1&password=password&age=17
 	e.GET("/api/user", getUserQuery)
 
-	e.POST("/api/user", postUserJson)
+	e.POST("/api/user", postUserForm)
+
+	e.POST("/api/user", func(c echo.Context) error {
+		u := new(User)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		return c.JSON(http.StatusCreated, u)
+		// or
+		// return c.XML(http.StatusCreated, u)
+	})
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
@@ -36,11 +46,18 @@ func getUserQuery(c echo.Context) error {
 
 //curl -F "name=Joe Smith" -F "password=password"  -F "age=17" http://localhost:1323/api/user
 // => name:Joe Smith, password:password, age=17
-func postUserJson(c echo.Context) error {
+func postUserForm(c echo.Context) error {
 	// Get name and email
 	name := c.FormValue("name")
 	password := c.FormValue("password")
 	age := c.FormValue("age")
 
 	return c.String(http.StatusOK, "name:"+name+"\n"+"password:"+password+"\n"+"age"+age)
+}
+
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Password string `json:"password" xml:"password" form:"password" query:"password"`
+	Age string `json:"age" xml:"age" form:"age" query:"age"`
+
 }
